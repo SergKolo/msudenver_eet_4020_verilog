a501e721a90366506da74a980af445583700f0a9
SergKolo <1047481448@qq.com>
2017-03-07 18:38:51 -0700

    Lab 2; testbench and waveform

--- LAB_1/VerilogAND.v
+++ LAB_1/VerilogAND.v
@@ -1,3 +1,8 @@
+/* Title: VerilogAND.v
+ * Description: Verilog implementation of 2 input AND gate
+ * Date: 2/23/2017
+ * Author: Sergiy Kolodyazhnyy
+ */
 module veriogAND(Fv,Av,Bv);
     input Av,Bv;
 	 output Fv;
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # msudenver_eet_4020_verilog
 materials for MSUD's course EET-4020 (Digital Circuits 3)
+
